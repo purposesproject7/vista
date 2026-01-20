c1154b4d82327704f4187329ee8bb1cd5fa81d23
Manikandan <81812564+mintnchocochip@users.noreply.github.com>
2026-01-20 11:16:15 +0530

    sadfsadf

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { API_BASE_URL } from "../shared/constants/config";
 
+console.log('🔍 Axios baseURL:', API_BASE_URL);
+
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
